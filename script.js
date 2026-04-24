d606102e685785c03f270ca32add366636bc165d
Zeadar <siiigsonik@gmail.com>
2026-04-24 20:22:25 +0200

    Add CV dialog to portfolio

---
+++ script.js
@@ -0,0 +1,17 @@
+const dialog = document.querySelector("#cv-window");
+const openButton = document.querySelector("#open-cv");
+const closeButton = document.querySelector("#close-cv");
+
+openButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+dialog.addEventListener("click", (event) => {
+  if (event.target === dialog) {
+    dialog.close();
+  }
+});
